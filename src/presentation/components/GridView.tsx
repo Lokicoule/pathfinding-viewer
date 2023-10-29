4147d02a185420e33cae33adbe9a1619d8191c3a
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-10-29 16:19:11 +0100

    feat: on mouse events

--- src/presentation/components/GridView.tsx
+++ src/presentation/components/GridView.tsx
@@ -3,6 +3,7 @@ import { Node } from "../../domain/entities/Node";
 import { NODE_PIXEL_SIZE } from "../../shared/constants";
 import { useCommand } from "../adapters/mediator/hooks/useCommand";
 import { useGrid } from "../hooks/useGrid";
+import useStateMap from "../hooks/useStateMap";
 import { concatClassNames } from "../utils/concatClassNames";
 
 type GridComponent = React.FC;
@@ -10,9 +11,32 @@ type GridComponent = React.FC;
 const GridView: GridComponent = () => {
   const grid = useGrid();
   const sendCommand = useCommand();
+  const nodes = useStateMap<string, Node>();
 
   const handleNodeClick = (node: Node) => {
     sendCommand(NodeInteractionCommand.name, new NodeInteractionCommand(node));
+  };
+
+  const handleMouseDown = (node: Node) => {
+    nodes.addEntry(node.id, node);
+  };
+
+  const handleMouseEnter = (node: Node) => {
+    if (nodes.Map.size > 0 && !nodes.Map.has(node.id)) {
+      nodes.addEntry(node.id, node);
+    } else if (nodes.Map.size > 0 && nodes.Map.has(node.id)) {
+      nodes.deleteEntry(node.id);
+    }
+  };
+
+  const handleMouseUp = () => {
+    Array.from(nodes.Map.values()).forEach((node) => {
+      sendCommand(
+        NodeInteractionCommand.name,
+        new NodeInteractionCommand(node)
+      );
+    });
+    nodes.clearMap();
   };
 
   return (
@@ -26,13 +50,25 @@ const GridView: GridComponent = () => {
                 "cell",
                 node.isStart() ? "start" : undefined,
                 node.isEnd() ? "end" : undefined,
-                node.isWall() ? "wall" : undefined
+                node.isWall() && !nodes.Map.has(node.id) ? "wall" : undefined,
+                nodes.Map.has(node.id) && !node.isWall() ? "wall" : undefined
               )}
               style={{
                 width: `${NODE_PIXEL_SIZE}px`,
                 height: `${NODE_PIXEL_SIZE}px`,
               }}
               onClick={() => handleNodeClick(node)}
+              onMouseDown={
+                !node.isStart() && !node.isEnd()
+                  ? () => handleMouseDown(node)
+                  : undefined
+              }
+              onMouseEnter={
+                !node.isStart() && !node.isEnd()
+                  ? () => handleMouseEnter(node)
+                  : undefined
+              }
+              onMouseUp={() => handleMouseUp()}
             ></div>
           ))}
         </div>
---
+++ src/presentation/hooks/useStateMap.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function useStateMap<K, V>(initialMap = new Map<K, V>()) {
+  const [map, setMap] = useState<Map<K, V>>(initialMap);
+
+  const addEntry = (key: K, value: V) => {
+    setMap(new Map(map).set(key, value));
+  };
+
+  const deleteEntry = (key: K) => {
+    const newMap = new Map(map);
+    newMap.delete(key);
+    setMap(newMap);
+  };
+
+  const clearMap = () => {
+    setMap(new Map());
+  };
+
+  return {
+    Map: map,
+    addEntry,
+    deleteEntry,
+    clearMap,
+  };
+}
+
+export default useStateMap;
