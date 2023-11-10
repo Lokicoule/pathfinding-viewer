9fd57152b9b894cb9efbbfc75b617dfc8a97cf47
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-11-10 13:21:01 +0100

    feat(application): use Graph datastructure instead of Node[][]

--- src/application/algorithms/maze/PrimsAlgorithm.ts
+++ src/application/algorithms/maze/PrimsAlgorithm.ts
@@ -1,19 +1,60 @@
+import { Graph } from "../../../domain/entities/Graph";
 import { Node } from "../../../domain/entities/Node";
 import { NodeType } from "../../../domain/enums/NodeType";
 import { Algorithm } from "../../../domain/interfaces/Algorithm";
 
 export class PrimsAlgorithm implements Algorithm {
-  private constructor(
-    private readonly grid: Node[][],
-    private readonly startNode: Node
-  ) {}
+  private readonly graph: Graph<Node>;
+  private readonly startNode: Node;
+  private readonly walls: Node[] = [];
 
-  public static create(grid: Node[][], startNode: Node): Algorithm {
-    return new PrimsAlgorithm(grid, startNode);
+  private constructor(graph: Graph<Node>, startNode: Node, walls: Node[] = []) {
+    this.graph = graph;
+    this.startNode = startNode;
+    this.walls = walls;
+  }
+
+  public static create(nodes: Node[][], startNode: Node): Algorithm {
+    const graph = this.createGraphFromNodes(nodes);
+    const walls = this.initializeWalls(nodes);
+    console.log(walls);
+    console.log(graph);
+    return new PrimsAlgorithm(graph, startNode, walls);
+  }
+
+  private static createGraphFromNodes(nodes: Node[][]): Graph<Node> {
+    const graph = new Graph<Node>();
+
+    for (const row of nodes) {
+      for (const node of row) {
+        if (!node.isStart() && !node.isEnd()) {
+          node.setWall();
+        }
+        graph.addVertex(node);
+      }
+    }
+
+    for (let i = 0; i < nodes.length; i++) {
+      for (let j = 0; j < nodes[i].length; j++) {
+        if (j < nodes[i].length - 1) {
+          if (!nodes[i][j].isStart() && !nodes[i][j].isEnd()) {
+            nodes[i][j].setWall();
+          }
+          graph.addEdge(nodes[i][j], nodes[i][j + 1]);
+        }
+        if (i < nodes.length - 1) {
+          if (!nodes[i][j].isStart() && !nodes[i][j].isEnd()) {
+            nodes[i][j].setWall();
+          }
+          graph.addEdge(nodes[i][j], nodes[i + 1][j]);
+        }
+      }
+    }
+
+    return graph;
   }
 
   public run(): Node[] {
-    const walls: Node[] = this.initializeWalls();
     const openNodes: Node[] = [this.startNode];
 
     while (openNodes.length) {
@@ -30,79 +71,69 @@ export class PrimsAlgorithm implements Algorithm {
           this.removeNode(openNodes, currentNode);
         }
 
-        this.updateWalls(randomNeighbor, currentNode, walls);
+        this.updateWalls(randomNeighbor);
       }
     }
 
-    return walls;
+    return this.walls;
   }
 
-  private initializeWalls(): Node[] {
-    const walls: Node[] = [];
+  private static initializeWalls(nodes: Node[][], walls: Node[] = []): Node[] {
+    if (!nodes.length) return walls;
 
-    for (const row of this.grid) {
-      for (const node of row) {
-        if (!node.isStart() && !node.isEnd()) {
-          node.setWall();
+    for (let i = 0; i < nodes.length; i++) {
+      for (let j = 0; j < nodes[i].length; j++) {
+        if (
+          (i === 0 ||
+            j === 0 ||
+            i === nodes.length - 1 ||
+            j === nodes[i].length - 1) &&
+          !nodes[i][j].isStart() &&
+          !nodes[i][j].isEnd()
+        ) {
           walls.push(
             Node.create({
               type: NodeType.Wall,
-              vector: node.getVector(),
+              vector: nodes[i][j].getVector(),
             })
           );
         }
       }
     }
 
-    return walls;
+    return this.initializeWalls(
+      nodes.slice(1, -1).map((row) => row.slice(1, -1)),
+      walls
+    );
   }
 
   private getNeighbors(node: Node): Node[] {
-    const neighbors: Node[] = [];
-    const { x, y } = node.getVector();
-
-    if (y > 0) neighbors.push(this.grid[y - 1][x]);
-    if (y < this.grid.length - 1) neighbors.push(this.grid[y + 1][x]);
-    if (x > 0) neighbors.push(this.grid[y][x - 1]);
-    if (x < this.grid[0].length - 1) neighbors.push(this.grid[y][x + 1]);
-
-    return neighbors.filter((neighbor) => neighbor.isWall());
+    return this.graph
+      .getEdges(node)
+      .map((vertex) => this.graph.getVertex(vertex).value)
+      .filter((neighbor) => neighbor.isWall());
   }
 
-  private chooseRandomNode<Node>(nodes: Node[]): Node {
+  private chooseRandomNode(nodes: Node[]): Node {
     const index = Math.floor(Math.random() * nodes.length);
     return nodes[index];
   }
 
-  private updateWalls(
-    randomNeighbor: Node,
-    currentNode: Node,
-    walls: Node[]
-  ): void {
+  private updateWalls(randomNeighbor: Node): void {
     randomNeighbor.setExplored();
-    walls.push(
+    this.walls.push(
       Node.create({
         type: NodeType.Explored,
         vector: randomNeighbor.getVector(),
       })
     );
-    walls.push(
-      Node.create({ type: NodeType.Wall, vector: randomNeighbor.getVector() })
-    );
 
-    const connectingNode =
-      this.grid[
-        Math.floor(
-          (randomNeighbor.getVector().y + currentNode.getVector().y) / 2
-        )
-      ][
-        Math.floor(
-          (randomNeighbor.getVector().x + currentNode.getVector().x) / 2
-        )
-      ];
+    const connectingNode = this.graph.getVertex(
+      this.graph.getEdges(randomNeighbor)[0]
+    ).value;
 
     if (!connectingNode.isStart() && !connectingNode.isEnd()) {
-      walls.push(
+      this.walls.push(
         Node.create({
           type: NodeType.Empty,
           vector: connectingNode.getVector(),
@@ -110,6 +141,13 @@ export class PrimsAlgorithm implements Algorithm {
       );
       connectingNode.setEmpty();
     }
+
+    this.walls.push(
+      Node.create({
+        type: NodeType.Wall,
+        vector: randomNeighbor.getVector(),
+      })
+    );
   }
 
   private removeNode(nodes: Node[], node: Node): void {
