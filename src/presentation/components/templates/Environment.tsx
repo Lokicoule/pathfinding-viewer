90eee0e58350c6f84b06bba279b1239544b31ccd
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-11-20 13:10:16 +0100

    style: margin top between grid and legend

--- src/presentation/components/templates/Environment.tsx
+++ src/presentation/components/templates/Environment.tsx
@@ -11,7 +11,7 @@ const Environment: EnvironmentComponent = () => {
       <Card.Body className="flex flex-col px-4 py-4 overflow-y-auto">
         <GridControls />
         <NodeGrid />
-        <Legend className="justify-end" />
+        <Legend className="justify-end mt-4" />
       </Card.Body>
     </Card>
   );
