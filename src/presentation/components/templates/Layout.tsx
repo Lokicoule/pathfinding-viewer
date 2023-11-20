03cc2caf024d66cb20ea5806c14f559107aba075
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-11-20 13:01:26 +0100

    style: title

--- src/presentation/components/templates/Layout.tsx
+++ src/presentation/components/templates/Layout.tsx
@@ -2,6 +2,7 @@ import {
   SIDEBAR_MAX_WIDTH,
   SIDEBAR_MIN_WIDTH,
 } from "../../../shared/constants";
+import { GithubIcon } from "../atoms/icons";
 import MazeAlgorithmSelector from "../molecules/algorithm/MazeAlgorithmSelector";
 import PathfindingAlgorithmSelector from "../molecules/algorithm/PathfindingAlgorithmSelector";
 import ResizableSidebar from "../molecules/layout/ResizableSidebar";
@@ -12,19 +13,23 @@ type LayoutProps = React.PropsWithChildren;
 type LayoutComponent = React.FC<LayoutProps>;
 
 const Layout: LayoutComponent = ({ children }) => {
-  const refresh = () => window.location.reload();
-
   return (
     <div className="flex flex-col h-screen">
       <div className="flex-1 flex flex-col sm:overflow-hidden">
         <header className="flex flex-col items-center px-4 pt-4 pb-1">
-          <div className="flex">
-            <h1
-              className="text-white text-center text-2xl font-semibold cursor-pointer font-primary"
-              onClick={refresh}
-            >
+          <div className="flex justify-center items-center w-full">
+            <h1 className="text-white text-center text-2xl font-semibold font-primary cursor-default">
               Pathfinding & Maze Generation Visualizer
             </h1>
+            <span className="text-white font-primary ml-2 mt-2 text-center text-xs ">
+              by{" "}
+              <a
+                className="hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r from-sky-400 to-blue-500 cursor-pointer"
+                href="https://github.com/Lokicoule/pathfinding-viewer"
+              >
+                Lokicoule
+              </a>
+            </span>
           </div>
         </header>
         <div className="flex flex-wrap sm:flex-nowrap sm:h-full space-x-1 justify-center">
