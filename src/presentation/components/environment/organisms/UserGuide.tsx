9b4b65562ffed921c3b7fa39af0dd2041f24ea8b
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-11-22 10:50:39 +0100

    style: user guide

--- src/presentation/components/environment/organisms/UserGuide.tsx
+++ src/presentation/components/environment/organisms/UserGuide.tsx
@@ -8,22 +8,31 @@ const UserGuide: UserGuideComponent = () => {
       isBlurred
       className="rounded-lg w-full overflow-y-auto bg-opacity-40 overflow-y-auto bg-[#184d67]"
     >
-      <Card.Body className="flex flex-col space-y-4 px-4 py-5 sm:p-6 ">
-        <div className="flex flex-col space-y-2 justify-between text-white">
-          <h2 className="text-md underline">Placing Walls:</h2>
-          <p className="text-sm">Drag to place walls on the grid.</p>
-
-          <h2 className="text-md underline">Moving Start and End Points:</h2>
-          <p className="text-sm">
-            Click on either the start or end point, then move it to another
-            cell.
-          </p>
-
-          <h2 className="text-md underline">Swapping Start and End Nodes:</h2>
-          <p className="text-sm">
-            Click on the start or end node, then click on the opposite node to
-            swap their positions.
-          </p>
+      <Card.Header className="flex flex-col px-4 py-5 sm:p-6">
+        <h1 className="text-lg font-semibold text-white text-center font-primary">
+          Hint!
+        </h1>
+      </Card.Header>
+      <Card.Body className="flex flex-col space-y-5 px-6 pb-6">
+        <div className="flex flex-col space-y-6 justify-around text-white text-justify">
+          <span className="flex flex-col space-y-2 ">
+            <h2 className="text-md underline">Placing Walls:</h2>
+            <p className="text-sm">Drag to place walls on the grid.</p>
+          </span>
+          <span className="flex flex-col space-y-2">
+            <h2 className="text-md underline">Moving Start and End Points:</h2>
+            <p className="text-sm">
+              Click on either the start or end point, then move it to another
+              cell.
+            </p>
+          </span>
+          <span className="flex flex-col space-y-2">
+            <h2 className="text-md underline">Swapping Start and End Nodes:</h2>
+            <p className="text-sm">
+              Click on the start or end node, then click on the opposite node to
+              swap their positions.
+            </p>
+          </span>
         </div>
       </Card.Body>
     </Card>
