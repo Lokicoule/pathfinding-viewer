81490a881f62a7b43783af44e2b45d6702e16dbb
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-11-28 22:19:49 +0100

    refactor: app root remove composition loading

--- src/presentation/app/AppRoot.tsx
+++ src/presentation/app/AppRoot.tsx
@@ -1,24 +1,15 @@
-import { CompositionRoot } from "@/bootstrapping/CompositionRoot";
 import { MediatorProvider } from "@ui/adapters/mediator";
-import { useEffect, useState } from "react";
-import { compositionRoot as composition } from "../../bootstrapping/bootstrap";
+import { compositionRoot } from "../../bootstrapping/bootstrap";
 import AppShell from "./AppShell";
 
 type AppRootComponent = React.FC;
 
 const AppRoot: AppRootComponent = () => {
-  const [compositionRoot, setCompositionRoot] =
-    useState<CompositionRoot | null>(null);
-
-  useEffect(() => {
-    setCompositionRoot(composition);
-  }, []);
-
-  return compositionRoot ? (
+  return (
     <MediatorProvider mediator={compositionRoot.mediator}>
       <AppShell />
     </MediatorProvider>
-  ) : null;
+  );
 };
 
 export default AppRoot;
