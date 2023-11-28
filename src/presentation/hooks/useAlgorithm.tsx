cb753c69c3f54844322cc10c40b800785d744e0e
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-11-28 19:48:09 +0100

    refactor: remove unused code

--- src/presentation/hooks/useAlgorithm.tsx
+++ src/presentation/hooks/useAlgorithm.tsx
@@ -1,11 +1,8 @@
+import { Algorithm } from "@/domain/algorithm";
 import { AlgorithmUpdatedEvent } from "@/domain/algorithm/events/AlgorithmUpdatedEvent";
 import { GetAlgorithmQuery } from "@/domain/algorithm/queries/GetAlgorithmQuery";
 import { useEventListener } from "../adapters/mediator/hooks";
-import { QueryResult, useQuery } from "../adapters/mediator/hooks/useQuery";
-import { getInitialAlgorithm } from "../helpers/algorithm";
-import { AlgorithmViewModel } from "../viewModels/AlgorithmViewModel";
-import { Algorithm } from "@/domain/algorithm";
-import { useState } from "react";
+import { useQuery } from "../adapters/mediator/hooks/useQuery";
 
 export const useAlgorithm = () => {
   const [query, callback] = useQuery<GetAlgorithmQuery, Algorithm>(
@@ -18,35 +15,4 @@ export const useAlgorithm = () => {
     loading: query.loading,
     error: query.error,
   };
-};
-
-export const useGetInitialAlgorithm = (algorithms: AlgorithmViewModel[]) => {
-  const [result, setResult] = useState<QueryResult<AlgorithmViewModel>>({
-    result: null,
-    loading: false,
-    error: null,
-  });
-  const [queryResult] = useQuery<GetAlgorithmQuery, Algorithm>(
-    new GetAlgorithmQuery()
-  );
-
-  try {
-    setResult((prev) => ({ ...prev, loading: true }));
-
-    if (queryResult.result) {
-      setResult({
-        loading: false,
-        error: null,
-        result: getInitialAlgorithm(algorithms, queryResult.result.value),
-      });
-    }
-  } catch (error) {
-    setResult((prev) => ({
-      ...prev,
-      error: error instanceof Error ? error : new Error("Unknown error"),
-      loading: false,
-    }));
-  }
-
-  return [result];
 };
